861f7c502a74345a49701f45679675ed7fb945f3
Juan Diego Caceres Munares <44043720+juandiego436@users.noreply.github.com>
2025-04-06 18:46:59 -0500

    Inicio de proyecto

---
+++ InspectionApiApp/InspectionType.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InspectionApiApp
+{
+
+    public class InspectionType
+    {
+        public int Id { get; set; }
+
+        [StringLength(20)]
+        public string InspectionName { get; set; } = string.Empty;
+    }
+}
